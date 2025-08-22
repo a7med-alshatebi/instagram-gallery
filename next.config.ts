176b46121cd517b126926c9b444ff0af63ba532c
Ahmed Alshatebi <ahmedalshatibi22@gmail.com>
2025-08-22 12:41:53 +0300

    Allow Instagram CDN domains for Next.js image optimization to fix gallery image loading errors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      'scontent-iad3-2.cdninstagram.com',
+      'scontent.cdninstagram.com',
+      'instagram.com',
+      'scontent.xx.fbcdn.net',
+    ],
+  },
 };
 
 export default nextConfig;
